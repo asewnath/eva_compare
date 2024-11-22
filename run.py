e77486638bec0d19b5ffb364b9f743ae5c50a236
asewnath <akira.sewnath@gmail.com>
2024-11-22 15:04:56 -0500

    more dev

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import os
 import yaml
 from dataclasses import dataclass
+from eva.eva_driver import eva
+from datetime import datetime as dt
+from datetime import timedelta
+
 
 @dataclass
 class SwellRun:
-    model_obs : dict
+    model_obs : list
     cycle_times : list
     swell_dir : str
     
@@ -14,43 +18,59 @@ def get_swell_run(swell_dir: str) -> SwellRun:
     cycle_times = os.listdir(swell_dir + "/run/")
     if not cycle_times:
         raise ValueError("No cycle times found in experiment.")
-
     models = os.listdir(ctrl_dir + "/run/" + cycle_times[0])
     if not models:
         raise ValueError("No models found in experiment.")
-
-    model_obs = {}
+    model_obs = []
     for model in models:
-        eva_dir = ctrl_dir + "/run/" + cycle_times[0] + model + "/eva/"
-        obs = os.listdir(ctrl_eva_dir)
+        tmp_dict = {}
+        eva_dir = ctrl_dir + "/run/" + cycle_times[0] + "/" + model + "/eva/"
+        obs = os.listdir(eva_dir)
         if not obs:
             raise ValueError("No observation list found in experiment")
-        model_obs["model"] = model
-        model_obs["obs"] = obs
+        tmp_dict["model"] = model
+        tmp_dict["obs"] = obs
+        model_obs.append(tmp_dict)
 
     return SwellRun(model_obs, cycle_times, swell_dir)
 
 
 def eva_compare(exp_run: SwellRun, ctrl_run: SwellRun, run_type: str) -> None:
 
-    config = yaml.safe_load("configs/IodaObsSpacePlots.yaml")
+    with open("configs/IodaObsSpacePlots.yaml", 'r') as stream:
+        config = yaml.safe_load(stream)
     # Find intersections between two experiments before running eva compare
-
+     
     # Create filenames for ctrl/exp and execute eva
-    for cycle_time in cycle_times:
-        for model_ob in model_obs:
+    for cycle_time in exp_run.cycle_times:
+        # Subtract 3 hours from datetime
+        dto = dt.strptime(cycle_time, "%Y%m%dT%H%M%SZ")
+        dto = dto - timedelta(hours=3)
+        datetime = dto.strftime("%Y%m%dT%H%M%SZ")
+
+        for model_ob in exp_run.model_obs:
+            model = model_ob["model"]
             obs_list = model_ob["obs"]
             for obs in obs_list:
-                filename = f"swell-{run_type}.{obs}.{cycle_time}.nc4"
+                filename = f"swell-{run_type}.{obs}.{datetime}.nc4"
                 ctrl_path = f"{ctrl_run.swell_dir}/run/{cycle_time}/{model}/{filename}"
-                exp_path  = f"{ctrl_run.swell_dir}/run/{cycle_time}/{model}/{filename}"
+                exp_path  = f"{exp_run.swell_dir}/run/{cycle_time}/{model}/{filename}"
                 # Update filenames in templates
+                for ind in range(len(config["datasets"])):
+                    if config["datasets"][ind]["name"] == "control":
+                        config["datasets"][ind]["filenames"] = [ctrl_path]
+                    elif config["datasets"][ind]["name"] == "experiment":
+                        config["datasets"][ind]["filenames"] = [exp_path]
+                    else:
+                        raise ValueError("datasets should be named control and experiment")
                 # Run eva
+                eva(config)
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 
-    template = yaml.safe_load("template.yaml")
+    with open("template.yaml", 'r') as stream:
+        template = yaml.safe_load(stream)
 
     # Get list of cycle times, models, and observations from control
     ctrl_dir = template["control_directory"]
@@ -61,5 +81,5 @@ if __name__ == "__main__"
     exp_swell_run = get_swell_run(exp_dir)
 
     # Generate comparison plots for swell runs
-    run_type = template["run_type"]
+    run_type = template["swell_run_type"]
     eva_compare(exp_swell_run, ctrl_swell_run, run_type)
